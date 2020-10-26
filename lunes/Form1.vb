899886d24d6a533c5d81f75fdd87cf20596644a2
maximohernandezga <maximo.hernandezga@elorrieta-errekamari.com>
2020-10-26 12:05:54 +0100

    Estoy en rama01

--- lunes/Form1.vb
+++ lunes/Form1.vb
@@ -3,5 +3,6 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Hola lunes")
         'Comentario1
+        'Estoy en rama01
     End Sub
 End Class
